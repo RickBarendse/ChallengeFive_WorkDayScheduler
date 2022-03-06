bd0b1dea76565266f12c5fbb3590278030148ad1
Rick Barendse <dazedeyes@yahoo.com>
2022-03-06 12:48:20 -0800

    color coding logic for time blocks based if it is in the past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-// display current day at top of scheduler
-$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
-$("#currentTime").text(moment().format("HH:MM"));
+// declare variables
+var now = moment();
 
+// display current day and time at top of scheduler
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+$("#currentTime").text(moment(now).format("HH:mm"));
+
+
+// color code time block based on whether it is in the past, present, or future
+function timeBlockColor () {
+    var hour = moment().hours();
+    console.log(hour)
+
+    $(".time-block").each(function () {
+        var currentHour = parseInt($(this).attr("id"));
+        console.log(currentHour);
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+
+timeBlockColor();
