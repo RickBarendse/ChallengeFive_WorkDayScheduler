23aeebdb3fcfbc261f8c775318f49c763a31875a
Rick Barendse <dazedeyes@yahoo.com>
2022-03-06 13:30:26 -0800

    save tasks to and retrieve tasks from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // declare variables
 var now = moment();
+var saveBtn = $(".saveBtn");
+
 
 // display current day and time at top of scheduler
 $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
@@ -25,5 +27,25 @@ function timeBlockColor () {
     })
 };
 
+// save tasks to local storage when save button is clicked
+saveBtn.on("click", function() {
+    var time = $(this).siblings(".hour").text();
+    var toDoText = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, toDoText);
+})
+
+// retrieve tasks from local storage on page reload
+function planMyDay () {
+    $(".hour").each(function() {
+        var currentHour = $(this).text();
+        var currentToDoText = localStorage.getItem(currentHour);
+
+        if(currentToDoText !== null) {
+            $(this).siblings(".description").val(currentToDoText);
+        }
+    });
+}
 
 timeBlockColor();
+planMyDay();
